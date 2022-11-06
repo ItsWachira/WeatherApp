61833640072a129b75269da6b180aeafb43f7892
ItsWachira <brianwachira79@gmail.com>
2022-11-06 11:55:12 +0300

    app styles

---
+++ index.js
@@ -0,0 +1,13 @@
+const wrapper= document.querySelector(".wrapper");
+inputpart = wrapper.querySelector(".input-part");
+infoTxt = inputpart.querySelector(".info-txt");
+inputField = inputpart.querySelector("input");
+ button= wrapper.querySelector("btn");
+
+
+
+
+locationBtn.addEventListener("click", () => {
+    console.log("clicked");
+   
+});
